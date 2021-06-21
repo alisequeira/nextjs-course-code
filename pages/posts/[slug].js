e7853339c78f8430635657d7c9244908b779a214
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-06-21 14:54:00 -0400

    Rendering Dynamic post pages and path

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,32 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-function PostDetailPage() {
-  return <PostContent />;
+function PostDetailPage(props) {
+  return <PostContent post={props.post} />;
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+}
+
+export async function getStaticPaths() {
+  const postsFileNames = getPostsFiles();
+  const slugs = postsFileNames.map((fileName) => fileName.replace(/\.md$/, ""));
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug } })),
+    fallback: false,
+  };
 }
 
 export default PostDetailPage;
