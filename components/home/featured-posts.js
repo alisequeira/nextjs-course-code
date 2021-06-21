1497beda0ae7f573267513edd0d1e106c4b1d5d3
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-06-21 12:30:18 -0400

    Rendering Dummy data

--- components/home/featured-posts.js
+++ components/home/featured-posts.js
@@ -1,8 +1,10 @@
 import classes from "./featured-posts.module.css";
-function FeaturedPosts() {
+import PostsGrid from "./posts/posts-grid";
+function FeaturedPosts(props) {
   return (
     <section className={classes.latest}>
       <h2>Featured Posts</h2>
+      <PostsGrid posts={props.posts} />
     </section>
   );
 }
--- components/home/posts/post-item.js
+++ components/home/posts/post-item.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import classes from "./post-item.module.css";
+import classes from "./posts-item.module.css";
 function PostItem(props) {
   const { title, image, excerpt, date, slug } = props.post;
   const formattedDate = new Date(date).toLocaleDateString("en-US", {
@@ -8,14 +8,20 @@ function PostItem(props) {
     month: "long",
     year: "numeric",
   });
-  const imagePath = `/images/posts/${slug}/${image}`;
-
+  const imagePath = `/images/post/${slug}/${image}`;
+  const linkPath = `/posts/${slug}`;
   return (
-    <li>
-      <Link>
+    <li className={classes.post}>
+      <Link href={linkPath}>
         <a>
           <div className={classes.image}>
-            <Image src={imagePath} alt={title} width={300} height={200} />
+            <Image
+              src={imagePath}
+              alt={title}
+              width={300}
+              height={200}
+              layout="responsive"
+            />
           </div>
           <div className={classes.content}>
             <h3>{title}</h3>
