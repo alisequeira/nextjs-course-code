f225e28e936b14fd9b403e121f985678c7fd28c5
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-06-21 16:05:07 -0400

    Sending data from the client to the api

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,23 +1,55 @@
+import { useState } from "react";
 import classes from "./contact-form.module.css";
 
 function ContactForm() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const sendMessageHandler = (event) => {
+    event.preventDefault();
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify({ email, name, message }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
   return (
     <section className={classes.contact}>
       <h1>How can I help you?</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Your email</label>
-            <input type="email" id="email" required />
+            <input
+              type="email"
+              id="email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className={classes.control}>
-            <label htmlFor="name">Your email</label>
-            <input type="text" id="name" required />
+            <label htmlFor="name">Your name</label>
+            <input
+              type="text"
+              id="name"
+              required
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
           </div>
         </div>
         <div className={classes.control}>
           <label htmlFor="message">Your message</label>
-          <textarea id="message" rows="5"></textarea>
+          <textarea
+            id="message"
+            rows="5"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
         </div>
         <div className={classes.actions}>
           <button>Send message</button>
