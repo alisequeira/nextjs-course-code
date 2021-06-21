0d79e26bef524a48334516f2d5cd6eaf0ae3aafd
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-06-21 11:18:35 -0400

    Adding The hero component

---
+++ components/home/Hero.js
@@ -0,0 +1,22 @@
+import classes from "./hero.module.css";
+import Image from "next/image";
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/max.png"
+          alt="An image showing Ali"
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>Hi, I'm Ali</h1>
+      <p>
+        I blog about web development - especially fronted frameworks like React.
+      </p>
+    </section>
+  );
+}
+export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
-function HomePage() {}
+import Hero from "../components/home/Hero";
+function HomePage() {
+  return (
+    <>
+      <Hero />
+      <FeaturedPosts />
+    </>
+  );
+}
 
 export default HomePage;
