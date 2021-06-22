60357e0f0c2cd30a5ec0982de3c50ea315e67fc8
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-06-21 21:37:27 -0400

    Adding Head data

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Hero from "../components/home/Hero";
 import FeaturedPosts from "../components/home/featured-posts";
 import { getFeaturedPost } from "../lib/posts-util";
@@ -5,6 +6,13 @@ import { getFeaturedPost } from "../lib/posts-util";
 function HomePage(props) {
   return (
     <>
+      <Head>
+        <title>Ali' Blog</title>
+        <meta
+          name="description"
+          content="I post about programing and web development."
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </>
